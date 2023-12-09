6c0300e2b2465ef8fda3d55778bcabd6423a2ba9
theMorshed <themorshedctg@gmail.com>
2023-12-09 22:41:00 +0600

    subarray sum problem solved with brute force algorithm

---
+++ subarray_sum.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, sum, maximum = INT_MIN;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++) cin >> arr[i];
+    cin >> k;
+
+    for (int i = 0; i <= n - k; i++) {
+        sum = 0;
+        for (int j = i; j < i + k; j++) {
+            sum += arr[j];
+        }
+        maximum = max(maximum, sum);
+    }
+
+    cout << maximum << endl;
+
+    return 0;
+}
