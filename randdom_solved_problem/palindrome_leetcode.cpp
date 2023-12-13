ba5f803e0dbdca54a17afd5061d00c6930bf4986
theMorshed <themorshedctg@gmail.com>
2023-12-13 10:05:19 +0600

    palindrome solved

---
+++ randdom_solved_problem/palindrome_leetcode.cpp
@@ -0,0 +1,40 @@
+// Leetcode: https://leetcode.com/problems/valid-palindrome/
+// solved at december 12, 2023. 11:00 pm
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int size = s.size();
+        string first;
+        bool is_palindrome = true;
+
+        for (int i = 0; i < size; i++)
+        {
+            if ((s[i] >= 'A' && s[i] <= 'Z') || (s[i] >= 'a' && s[i] <= 'z') || (s[i] >= '0' && s[i] <= '9'))
+            {
+                if (s[i] >= 'A' && s[i] <= 'Z')
+                {
+                    first.push_back(char(s[i] + 32));
+                }
+                else
+                {
+                    first.push_back(s[i]);
+                }
+            }
+        }
+
+        int first_size = first.size();
+
+        for (int i = 0, j = first_size - 1; i < first_size / 2; i++, j--)
+        {
+            if (first[i] != first[j])
+            {
+                is_palindrome = false;
+                break;
+            }
+        }
+
+        return is_palindrome;
+    }
+};
