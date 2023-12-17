41193c88c64bee1cc80cacce7074e0a5c38be480
theMorshed <themorshedctg@gmail.com>
2023-12-17 14:34:00 +0600

    maximum number of vowels leetcode

---
+++ random_solved_problem/maximum_number_of_vowels_leetcode.cpp
@@ -0,0 +1,38 @@
+/*
+Link: https://leetcode.com/problems/maximum-number-of-vowels-in-a-substring-of-given-length/
+Solved at 17.12.23 2:33PM
+*/
+class Solution
+{
+public:
+    int maxVowels(string s, int k)
+    {
+        int n = s.size();
+        int maximum = INT_MIN;
+        int i = 0, j = 0, vowel_count = 0;
+
+        while (j < n)
+        {
+            if (s[j] == 'a' || s[j] == 'e' || s[j] == 'i' || s[j] == 'o' || s[j] == 'u')
+            {
+                vowel_count++;
+            }
+            if (j < k - 1)
+            {
+                j++;
+            }
+            else
+            {
+                maximum = max(maximum, vowel_count);
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u')
+                {
+                    vowel_count--;
+                }
+                i++;
+                j++;
+            }
+        }
+
+        return maximum;
+    }
+};
